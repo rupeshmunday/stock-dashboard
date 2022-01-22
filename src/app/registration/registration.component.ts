606da69efc2c794b40bb6ec70a2bf97ddbcf8043
Rupesh Kumar <pra6305@gmail.com>
2022-01-22 18:29:22 +0530

    updated angular components

--- src/app/registration/registration.component.ts
+++ src/app/registration/registration.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-registration',
@@ -7,9 +8,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistrationComponent implements OnInit {
 
-  constructor() { }
+  registerForm : FormGroup;
+  submitted = false;
 
-  ngOnInit(): void {
+  constructor( private formbuilder:FormBuilder) { }
+
+  ngOnInit(){
+    this.registerForm = this.formbuilder.group({
+      firstName : ["" , Validators.required],
+      lastName : ["" , Validators.required],
+      mobNo : ["" , Validators.required],
+      email : ["" ,[ Validators.required , Validators.email]],
+      password : ["" , Validators.required],
+      acceptAndC : [false , Validators.required]
+    });
   }
+
+  get h() {
+    return this.registerForm.controls;
+  }
+
+  onSubmit () {
+    this.submitted = true;
+    if (this.registerForm.invalid) {
+      return ;
+    }
+    console.table(this.registerForm.value);
+    console.table(this.registerForm);
+    alert("Success sign up\n" + JSON.stringify(this.registerForm.value))
+  }
+  OnReset() {
+    this.submitted = false;
+    this.registerForm.reset();
+  }
+  
 
 }
--- src/app/user-profile/user-profile.component.ts
+++ src/app/user-profile/user-profile.component.ts
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-profile.component.css']
 })
 export class UserProfileComponent implements OnInit {
+  userName = 'Rupesh';
 
   constructor() { }
 
